40afac84da3214f4080ac4f686c14472e99ff67e
abdounikarim <abdounikarim@gmail.com>
2019-09-18 16:40:02 +0200

    Tweak your Form based on the Underlying Data

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -43,7 +43,9 @@ class ArticleAdminController extends AbstractController
      */
     public function edit(Article $article, Request $request, EntityManagerInterface $em)
     {
-        $form = $this->createForm(ArticleFormType::class, $article);
+        $form = $this->createForm(ArticleFormType::class, $article, [
+            'include_published_at' => true
+        ]);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $em->persist($article);
